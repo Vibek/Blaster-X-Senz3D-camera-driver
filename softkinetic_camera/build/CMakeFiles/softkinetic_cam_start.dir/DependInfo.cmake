9b0341bc68261f59f823bf9b8c8f8a512aebdec7
Vibekananda Dutta <vibek4989@gmail.com>
2017-02-16 19:49:28 +0100

    blaster-X driver imoprved

---
+++ softkinetic_camera/build/CMakeFiles/softkinetic_cam_start.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vibek/softkinetic_camera/src/softkinetic_start.cpp" "/home/vibek/softkinetic_camera/build/CMakeFiles/softkinetic_cam_start.dir/src/softkinetic_start.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ softkinetic_camera/build/CMakeFiles/softkinetic_cam_start.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/softkinetic_cam_start.dir/src/softkinetic_start.cpp.o"
+  "../bin/softkinetic_cam_start.pdb"
+  "../bin/softkinetic_cam_start"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/softkinetic_cam_start.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
